156d4d99f207d0728bd139f4a7b72db3d14fb1c8
sachindrck <sachindrck25@gmail.com>
2020-01-05 23:19:33 +0530

    Added wordcloud

---
+++ wordcloud.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+text = "sachin saurabh anurag dd vv vv vv vv vv vv vv vv vv vv vv vv vv vv vv vv vv vv ankita nisha neha surabhi suman vv lalit sachin sachin sachin sachin sachin sachin sachin sachin sachin sachin sachin sachin sachin sachin"
+
+cloud = WordCloud(background_color="white").generate(text)
+
+plt.imshow(cloud)
+plt.axis('off')
+plt.show();
+
+
+#end
+
+
+
